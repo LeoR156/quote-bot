cce51e6045362a495edf653dc7126dc24dabe061
LeoR156 <277007013+LeoR156@users.noreply.github.com>
2026-09-05 13:36:16 +0500

    Check scan

---
+++ src/vuln_test.py
@@ -0,0 +1,15 @@
+import sqlite3
+def find_user_bad_practice(username: str):
+
+    conn = sqlite3.connect("fake_database.db")
+    cursor = conn.cursor()
+    
+
+    malicious_query = f"SELECT * FROM users WHERE name = '{username}'" # Vulnerability!
+    
+
+    cursor.execute(malicious_query)
+    
+    results = cursor.fetchall()
+    conn.close()
+    return results
