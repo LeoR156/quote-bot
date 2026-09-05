dddb226b91aa246aedcd1c1f50f566395e43aaac
LeoR156 <277007013+LeoR156@users.noreply.github.com>
2026-09-05 13:42:59 +0500

    Check scan v2

--- src/vuln_test.py
+++ src/vuln_test.py
@@ -1,13 +1,12 @@
 import sqlite3
-def find_user_bad_practice(username: str):
 
+
+def find_user_bad_practice(username: str):
     conn = sqlite3.connect("fake_database.db")
     cursor = conn.cursor()
     
-
     malicious_query = f"SELECT * FROM users WHERE name = '{username}'" # Vulnerability!
     
-
     cursor.execute(malicious_query)
     
     results = cursor.fetchall()
